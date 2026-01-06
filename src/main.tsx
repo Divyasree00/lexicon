33138ea1cb0e9b63a1b4f440f8a8b8e72e126e31
Divyasree00 <your-email@gmail.com>
2026-01-06 19:32:23 +0530

    test4

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const basename =
-  import.meta.env.MODE === "production" ? "/lexicon" : "/";
-
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={basename}>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
