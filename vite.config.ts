f087ef22a1bd1e2d10a31b55648a7c5def04207c
Divyasree00 <your-email@gmail.com>
2026-01-06 19:37:35 +0530

    test5

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/lexicon/", // 🔴 MUST MATCH REPO NAME
+  base: "/lexicon/",
   plugins: [react()],
   resolve: {
     alias: {
